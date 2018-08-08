7e7ef1c387ae5a3ecfb53c09397e5115ff8e8121
Andre Luis Sampaio <andreluisampaio@gmail.com>
2018-08-08 20:39:05 -0300

    endpoint pedidos

---
+++ src/main/java/com/sampalu/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.sampalu.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sampalu.cursomc.domain.Pedido;
+import com.sampalu.cursomc.repositories.PedidoRepository;
+import com.sampalu.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
