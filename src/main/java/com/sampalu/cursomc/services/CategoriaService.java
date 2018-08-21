9a47209bc8c1bafbc897a97c3b59db5d614c1dfa
Andre Luis Sampaio <andreluisampaio@gmail.com>
2018-08-21 01:52:13 -0300

    validacao com bean validation

--- src/main/java/com/sampalu/cursomc/services/CategoriaService.java
+++ src/main/java/com/sampalu/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sampalu.cursomc.domain.Categoria;
+import com.sampalu.cursomc.dto.CategoriaDTO;
 import com.sampalu.cursomc.repositories.CategoriaRepository;
 import com.sampalu.cursomc.services.exceptions.DataIntegrityException;
 import com.sampalu.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
